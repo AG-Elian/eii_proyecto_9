e36cef515540bddaa1fd72de7dc6cca8f39e5ce5
AG-Elian <aramallog.elian@gmail.com>
2024-11-12 09:13:35 -0300

    Se adicionan cambios en el banco de trabajo de ram_512x8 y reg_32x32

--- src/sim_ram_512x8.vhd
+++ src/sim_ram_512x8.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use std.env.finish;
+use work.util_sim.all;
 
 entity sim_ram_512x8 is
 end sim_ram_512x8;
@@ -19,22 +20,43 @@ architecture sim of sim_ram_512x8 is
       dat_r : out std_logic_vector(7 downto 0)
     );
   end component; -- ram_512x8
-  signal dir_r, dir_w : std_logic_vector(8 downto 0);
+  signal dir, : std_logic_vector(8 downto 0);
   signal dat_w : std_logic_vector(7 downto 0); 
   signal dat_r : std_logic_vector(7 downto 0);
-  signal hab_r, hab_w, clk_r, clk_w : std_logic;
+  signal hab_r, hab_w, clk: std_logic;
 begin
   -- Dispositivo bajo prueba
-  dut : ram_512x8 port map (clk_w=>clk_w,clk_r=>clk_r,dat_r=>dat_r,
-                      dat_w=>dat_w, hab_r=>hab_r,hab_w=>hab_w,dir_r=>dir_r,dir_w=>_dir_w);
+  dut : ram_512x8 port map (clk_w=>clk,clk_r=>clk,dat_r=>dat_r,
+                      dat_w=>dat_w, hab_r=>hab_r,hab_w=>hab_w,dir_r=>dir,dir_w=>dir);
 
-  excitaciones: process
-  begin
-    for i in 0 to (2**entradas'length)-1 loop
-      entradas <= std_logic_vector(to_unsigned(i,entradas'length));
+  reloj: process
+    begin
+      clk<='0';
       wait for 1 ns;
-    end loop;
-    wait for 1 ns; -- Espera extra antes de salir
-    finish;
+      clk<='1';
+      wait for 1 ns;
+      end process
+  excitaciones: process
+
+    variable aleatorio : aleatorio_t;
+    procedure sig_ciclo is
+      begin
+        wait until rising_edge(clk);
+        wait for 0.5 ns;
+        end procedure;
+  begin
+    for k in 0 to 10 loop
+      dir <= aleatorio.genera_vector(9);
+      dat_w <= (others '0');
+      hab <= '1';
+      sig ciclo;
+      dat_w <= aleatorio.genera_vector(9);
+      hab_w <= aleatorio.genera_bit;
+      sig_ciclo;
+      hab_w <= '0';
+      sig_ciclo;
+      end loop;
+      sig_ciclo;
+      finish;
   end process; -- excitaciones
 end sim;
--- src/sim_reg_32x32.vhd
+++ src/sim_reg_32x32.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use std.env.finish;
+use work.util_sim.all;
 
 entity sim_reg_32x32 is
 end sim_reg_32x32;
