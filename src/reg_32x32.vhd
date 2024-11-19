2168ec73208d82bb9f84e93e26b88cbdb968b3ef
AG-Elian <aramallog.elian@gmail.com>
2024-11-18 23:19:46 -0300

    Modificado el archivo reg_32x32 y su archivo de simulacion

--- src/reg_32x32.vhd
+++ src/reg_32x32.vhd
@@ -5,14 +5,14 @@ use IEEE.numeric_std.all;
 entity reg_32x32 is
   port (
     clk, hab_w : in  std_logic;
-    dir_w, dir_r1, dir_r2 : in  std_logic_vector(4 downto 0);
-    dat_w : in std_logic_vector(3 downto 0);
-    dat_r1, dat_r2 : out std_logic_vector(3 downto 0)
+    dir_w, dir_r1, dir_r2 : in  std_logic_vector(4 downto 0); -- direcciones de lectura/escritura
+    dat_w : in std_logic_vector(31 downto 0); -- datos de escritura
+    dat_r1, dat_r2 : out std_logic_vector(31 downto 0) --datos de lectura
   );
 end reg_32x32;
 
 architecture behavioral of reg_32x32 is
-  type mem_t is array(0 to 31) of std_logic_vector(3 downto 0);
+  type mem_t is array(0 to 31) of std_logic_vector(31 downto 0);
   signal mem : mem_t:=(others=>x"0");
 begin
   puertos : process (clk)
@@ -21,6 +21,8 @@ begin
       if hab_w = '1' then
         mem(to_integer(unsigned(dir_w)))<=dat_w;
         end if;
+        dat_r1<=mem(to_integer(unsigned(dir_r1)));
+        dat_r2<=mem(to_integer(unsigned(dir_r2)));
       end if;
     end process;
 end behavioral;
--- src/sim_reg_32x32.vhd
+++ src/sim_reg_32x32.vhd
@@ -10,27 +10,49 @@ end sim_reg_32x32;
 architecture sim of sim_reg_32x32 is
   component reg_32x32 is
     port (
-      clk, hab_w : in  std_logic;
+      clk, hab_w: in  std_logic;
       dir_w, dir_r1, dir_r2 : in  std_logic_vector(4 downto 0);
-      dat_w : in std_logic_vector(3 downto 0);
-      dat_r1, dat_r2 : out std_logic_vector(3 downto 0)
+      dat_w : in std_logic_vector(31 downto 0);
+      dat_r1, dat_r2 : out std_logic_vector(31 downto 0)
     );
   end component; -- reg_32x32
-  signal dir : std_logic_vector(8 downto 0);
-  signal dat_w : std_logic_vector(7 downto 0); 
-  signal dat_r : std_logic_vector(7 downto 0);
+
+  signal dir,dir_r1,dir_r2 : std_logic_vector(4 downto 0);
+  signal dat_w : std_logic_vector(31 downto 0); 
+  signal dat_r1, dat_r2 : std_logic_vector(31 downto 0);
   signal hab_w, clk: std_logic;
 begin
   -- Dispositivo bajo prueba
-  dut : reg_32x32 port map ();
+  dut : reg_32x32 port map (clk=>clk,dir_w=>dir,dir_r1=>dir_r1,dir_r2=>dir_r2,dat_w=>dat_w,dat_r1=>dat_r1,dat_r2=>dat_r2,hab_w=>hab_w);
 
-  excitaciones: process
-  begin
-    for i in 0 to (2**entradas'length)-1 loop
-      entradas <= std_logic_vector(to_unsigned(i,entradas'length));
+  reloj: process
+    begin
+      clk<='0';
       wait for 1 ns;
-    end loop;
-    wait for 1 ns; -- Espera extra antes de salir
-    finish;
+      clk<='1';
+      wait for 1 ns;
+      end process;
+  excitaciones: process
+  
+  variable aleatorio : aleatorio_t;
+    procedure sig_ciclo is
+      begin
+        wait until rising_edge(clk);
+        wait for 0.5 ns;
+        end procedure;
+  begin
+    for k in 0 to 10 loop
+      dir <= aleatorio.genera_vector(5);
+      dat_w <= (others=>'0');
+      hab_w <= '1';
+      sig_ciclo;
+      dat_w <= aleatorio.genera_vector(32);
+      hab_w <= aleatorio.genera_bit;
+      sig_ciclo;
+      hab_w <= '0';
+      sig_ciclo;
+      end loop;
+      sig_ciclo;
+      finish;
   end process; -- excitaciones
 end sim;
